bded869ead70790188b1d1a43527b97a21ba1cfd
Astol <DavidTeinert344@t-online.de>
2023-11-28 12:17:21 +0100

    added an Toggle Button for testing the Player Movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,13 +1,54 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    #region Members
+    #region Movement
+    [Header("Movement" , order = 1)]
     [SerializeField]
     private float movementSpeed;
+    [SerializeField]
+    private float movementSpeedLerp;
+    [SerializeField]
+    private float lerpTime = 0.1f;
+    #endregion
+
+    #region Toggle Lerp
+    [Header("Use Lerp", order = 2)]
+    [SerializeField]
+    private Toggle toggle;
+    private bool useLerp;
+    #endregion
+    #endregion
+
+    private Rigidbody2D rb;
+    private Vector2 targetPosition;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        targetPosition = transform.position;
+
+        toggle.onValueChanged.AddListener(delegate { ToggleValueChanged(toggle); });
+    }
+
+    private void ToggleValueChanged(Toggle toggle)
+    {
+        useLerp = toggle.isOn;
+        Debug.Log($"toggle State: {toggle.isOn}");
+    }
 
     void Update()
     {
-        Movement();
+        if(useLerp != true)
+        {
+            Movement();
+        }
+        MovementWithLerp();
+
     }
 
     private void Movement()
@@ -18,5 +59,25 @@ public class Player : MonoBehaviour
         Vector2 movement = new Vector2(horizontalInput, verticalInput);
 
         transform.Translate(movement * movementSpeed * Time.deltaTime);
+    }
+
+    private void MovementWithLerp()
+    {
+        HandleInput();
+        PlayerMovementLerp();
+    }
+
+    private void HandleInput()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        targetPosition = new Vector2(transform.position.x + horizontal, transform.position.y + vertical);
+    }
+
+    private void PlayerMovementLerp()
+    {
+        Vector2 newPosition = Vector2.Lerp(rb.position, targetPosition, lerpTime * movementSpeedLerp);
+        rb.MovePosition(newPosition);
     }
 }
