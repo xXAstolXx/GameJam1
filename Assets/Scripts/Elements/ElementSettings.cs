e5fcfdcd64f6db13f6549d33107757935c76f3a1
henrylhee <66679374+henrylhee@users.noreply.github.com>
2023-11-30 14:36:31 +0100

    add stuff

--- Assets/Scripts/Elements/ElementSettings.cs
+++ Assets/Scripts/Elements/ElementSettings.cs
@@ -9,10 +9,6 @@ public class ElementSettings : MonoBehaviour
     {
         get
         {
-            if (instance == null)
-            {
-                instance = new ElementSettings();
-            }
             return instance;
         }
     }
@@ -20,20 +16,25 @@ public class ElementSettings : MonoBehaviour
     public List<Element> elements;
 
     public Dictionary<ElementTransform, ElementTypes> transformElements = new Dictionary<ElementTransform, ElementTypes>()
-    { { new ElementTransform(ElementTypes.FIRE, ElementTypes.ICE), ElementTypes.WATER},
-        {new ElementTransform(ElementTypes.FIRE, ElementTypes.WATER), ElementTypes.STEAM},
-        {new ElementTransform(ElementTypes.WATER, ElementTypes.FIRE), ElementTypes.STEAM},
-        {new ElementTransform(ElementTypes.ICE, ElementTypes.FIRE), ElementTypes.WATER},
-        {new ElementTransform(ElementTypes.FIRE, ElementTypes.STEAM), ElementTypes.FIRE},
-        {new ElementTransform(ElementTypes.STEAM, ElementTypes.FIRE), ElementTypes.FIRE},
-        {new ElementTransform(ElementTypes.ICE, ElementTypes.WATER), ElementTypes.ICE},
-        {new ElementTransform(ElementTypes.WATER, ElementTypes.ICE), ElementTypes.ICE},
-        {new ElementTransform(ElementTypes.ICE, ElementTypes.STEAM), ElementTypes.STEAM},
-        {new ElementTransform(ElementTypes.STEAM, ElementTypes.ICE), ElementTypes.STEAM},
-        {new ElementTransform(ElementTypes.WATER, ElementTypes.STEAM), ElementTypes.STEAM},
-        {new ElementTransform(ElementTypes.STEAM, ElementTypes.WATER), ElementTypes.STEAM},
+    { { new ElementTransform(ElementTypes.FIRE, ElementTypes.ICE), ElementTypes.WATER
+},
+        { new ElementTransform(ElementTypes.FIRE, ElementTypes.WATER), ElementTypes.STEAM},
+        { new ElementTransform(ElementTypes.WATER, ElementTypes.FIRE), ElementTypes.STEAM},
+        { new ElementTransform(ElementTypes.ICE, ElementTypes.FIRE), ElementTypes.WATER},
+        { new ElementTransform(ElementTypes.FIRE, ElementTypes.STEAM), ElementTypes.FIRE},
+        { new ElementTransform(ElementTypes.STEAM, ElementTypes.FIRE), ElementTypes.FIRE},
+        { new ElementTransform(ElementTypes.ICE, ElementTypes.WATER), ElementTypes.ICE},
+        { new ElementTransform(ElementTypes.WATER, ElementTypes.ICE), ElementTypes.ICE},
+        { new ElementTransform(ElementTypes.ICE, ElementTypes.STEAM), ElementTypes.STEAM},
+        { new ElementTransform(ElementTypes.STEAM, ElementTypes.ICE), ElementTypes.STEAM},
+        { new ElementTransform(ElementTypes.WATER, ElementTypes.STEAM), ElementTypes.STEAM},
+        { new ElementTransform(ElementTypes.STEAM, ElementTypes.WATER), ElementTypes.STEAM},
     };
 
+    private void Start()
+    {
+        instance = GetComponent<ElementSettings>();
+    }
 
     public Element GetElementSettings(ElementTypes element){
         foreach(Element obj in elements)
